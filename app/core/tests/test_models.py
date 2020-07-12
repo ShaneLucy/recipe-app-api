3e4f69f420dc0e3a4eb031bb7eb899628c97df63
Shane Lucy <shanelucy01@protonmail.com>
2020-07-12 15:06:44 +0100

    Added list tags endpoint

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -2,6 +2,12 @@ from django.test import TestCase
 # importing user model like this means if the user models is changed then
 # it can be updated throught the program by changing settings
 from django.contrib.auth import get_user_model
+from core import models
+
+
+def sample_user(email='test23@tesmail.com', password='password123456'):
+    """Create a sample user"""
+    return get_user_model().objects.create_user(email, password)
 
 
 class ModelTests(TestCase):
@@ -38,3 +44,12 @@ class ModelTests(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_tag_str(self):
+        """Test the tag string representation"""
+        tag = models.Tag.objects.create(
+            user=sample_user(),
+            name='Vegan'
+        )
+
+        self.assertEqual(str(tag), tag.name)
